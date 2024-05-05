d6b0316c4ff2bf953e0bee4a4ce57e13be68083d
Ossai6 <calebossaiakpe@gmail.com>
2024-05-05 18:32:50 +0000

    added new files

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,13 @@
+# configuring ssh client to use private key and refuse password authentication.
+
+File_line { ‘disable_password_authentication’:
+  Path  => ‘/etc/ssh/ssh_config’,
+  Line  => ‘PasswordAuthentication no’,
+  Match => ‘^PasswordAuthentication’,
+}
+
+File_line { ‘set_ssh_identity_file’:
+  Path  => ‘/etc/ssh/ssh_config’,
+  Line  => ‘IdentityFile ~/.ssh/school’,
+  Match => ‘^IdentityFile’,
+}
